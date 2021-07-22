157f41ccaafb7cca39861d4b1cd5bdc4612c7543
Daniel  Pan <danielcpan.dev@gmail.com>
2021-07-21 20:34:13 -0700

    in progress

--- TypeScript/1.7_RotateMatrix.ts
+++ TypeScript/1.7_RotateMatrix.ts
@@ -2,7 +2,32 @@
 // bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 
 const rotateMatrix = (matrix: number[][]): number[][] => {
-  return [[0]];
+  const rows: number = matrix[0].length;
+  const cols: number = matrix.length;
+
+  printMatrix(matrix);
+
+  for (let i: number = 0; i < rows - 1; i++) {
+    for (let j: number = 0; j < cols - 1; j++) {
+      const temp: number = matrix[i][j];
+      matrix[i][j] = matrix[rows - 1 - j][i];
+      matrix[rows - 1 - j][i] = matrix[rows - 1][cols - 1 - j];
+      matrix[rows - 1][cols - 1 - j] = matrix[i + j][cols - 1];
+      matrix[i + j][cols - 1] = temp;
+
+      // console.log("matrix:", matrix);
+      printMatrix(matrix);
+    }
+  }
+
+  return matrix;
+};
+
+const printMatrix = (matrix: number[][]): void => {
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i]);
+  }
+  console.log();
 };
 
 // TEST CASES:
@@ -13,17 +38,23 @@ console.log(
     [7, 8, 9],
   ])
 );
-console.log(
-  rotateMatrix([
-    [5, 1, 9, 11],
-    [2, 4, 8, 10],
-    [13, 3, 6, 7],
-    [15, 14, 12, 16],
-  ])
-);
-console.log(
-  rotateMatrix([
-    [1, 2],
-    [3, 4],
-  ])
-);
+// [
+//   [7, 4, 1],
+//   [8, 5, 2],
+//   [9, 6, 3],
+// ];
+
+// console.log(
+//   rotateMatrix([
+//     [5, 1, 9, 11],
+//     [2, 4, 8, 10],
+//     [13, 3, 6, 7],
+//     [15, 14, 12, 16],
+//   ])
+// );
+// console.log(
+//   rotateMatrix([
+//     [1, 2],
+//     [3, 4],
+//   ])
+// );
